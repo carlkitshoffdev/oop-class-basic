4f7bfa6555bb670859c0e3bbce4ef315f37103dc
carlkitshoff <carlkitshoff@gmail.com>
2017-09-05 15:39:51 +0200

    Core folder containing class file

---
+++ core/oop.php
@@ -0,0 +1,18 @@
+<?php
+
+    class oop {
+
+        public function __construct() {
+
+            // basic vars in here
+
+        }
+
+        public function print_name( $name = '', $surname = '' ) {
+
+            return $name . ' ' . $surname;
+
+        }
+
+
+    }
